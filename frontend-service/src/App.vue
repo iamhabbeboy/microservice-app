459ff4aa938c79a71e39806838cb382dbbbce01a
Abiodun Azeez <iamhabbeboy@gmail.com>
2022-10-13 00:59:13 +0100

    init commit

--- frontend-service/src/App.vue
+++ frontend-service/src/App.vue
@@ -1,3 +1,27 @@
 <template>
-  <router-view></router-view>
+  <div class="">
+    <div class="w-full md:w-1/2 mx-auto mt-10">
+      <h1 class="text-2xl border-b-2">Home</h1>
+      <div>
+        <div class="flex mt-4">
+          <div class="w-6/12 h-64">
+            <h3 class="font-bold">Payload</h3>
+            <div class="w-full h-full bg-gray-200">
+                sdlfkdjf
+            </div>
+          </div>
+          <div class="w-6/12">
+            <h3 class="font-bold">Output</h3>
+               <div class="w-full h-full bg-gray-400">
+                sdlfkdjf
+            </div>
+          </div>
+        </div>
+        <div class="mt-10">
+          <button class="mr-3 p-3 bg-blue-600 text-white hover:bg-blue-500">Test Broker</button>
+          <button class="mr-3 p-3 bg-purple-600 text-white hover:bg-purple-500">Test Logger</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
